0f7844247caaf1192dd1e3b999985dfdc725f63b
Sarim khan <sarimk80@gmail.com>
2023-07-22 21:02:38 +0500

    Initial Commit

---
+++ VisionFrameworkApp/VisionFrameworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionFrameworkAppApp.swift
+//  VisionFrameworkApp
+//
+//  Created by sarim khan on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisionFrameworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
